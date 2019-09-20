a36b321129996a44f56b300aa9dc71e561079594
Brandon Green <jailhousejordans@gmail.com>
2019-09-20 13:06:54 -0500

    almost

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,15 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  //return <span>todo: character</span>;
+  console.log(props)
+  return (
+    <div>
+      <img src={props.image} />
+      <div>Name: {props.name}</div>
+      <div>Species: {props.species}</div>
+      <div>Status: {props.status}</div>
+
+    </div>
+  )
 }
