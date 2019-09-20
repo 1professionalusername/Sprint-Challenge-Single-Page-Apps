f05e4f70e08ff89b645eaf062155eba548f9fa5f
Brandon Green <jailhousejordans@gmail.com>
2019-09-20 17:16:20 -0500

    styled

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,15 +1,23 @@
 import React from "react";
+import styled from 'styled-components'
+
+const Card = styled.div`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+  background-color: black;
+`;
 
 export default function CharacterCard(props) {
   //return <span>todo: character</span>;
   console.log(props)
   return (
-    <div>
+    <Card>
       <img src={props.image} />
       <div>Name: {props.name}</div>
       <div>Species: {props.species}</div>
       <div>Status: {props.status}</div>
 
-    </div>
+    </Card>
   )
 }
